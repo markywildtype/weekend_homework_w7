455560e72bb8b2f2e63e8ade9488705157fb47c6
Mark Blanford <markytorture@hotmail.com>
2018-01-19 11:29:49 +0000

    initial commit

---
+++ src/main/java/Instruments/Instrument.java
@@ -0,0 +1,12 @@
+package Instruments;
+
+public abstract class Instrument {
+
+    private InstrumentType instrumentType;
+
+    public Instrument(InstrumentType instrumentType){
+
+
+    }
+
+}
---
+++ src/test/java/InstrumentTests/GuitarTest.java
@@ -0,0 +1,17 @@
+package InstrumentTests;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class GuitarTest {
+
+    Guitar guitar;
+
+    @Before
+    public void before(){
+        guitar = new Guitar(InstrumentType.STRINGED);
+    }
+
+    @Test
+    public void
+}
