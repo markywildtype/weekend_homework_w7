14153c3f7e29c1da08efc4aface077cf5e7cd079
Mark Blanford <markytorture@hotmail.com>
2018-01-19 15:40:43 +0000

    Added Other class.

---
+++ src/main/java/Accessories/Other.java
@@ -0,0 +1,8 @@
+package Accessories;
+
+public class Other extends Accessory{
+
+    public Other(String description, double buyPrice, double sellPrice) {
+        super(description, buyPrice, sellPrice);
+    }
+}
--- src/test/java/AccessoriesTests/OtherTest.java
+++ src/test/java/AccessoriesTests/OtherTest.java
@@ -1,4 +1,37 @@
 package AccessoriesTests;
 
+import Accessories.Other;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class OtherTest {
+
+    Other other;
+
+    @Before
+    public void before(){
+        other = new Other("Footrest", 9.30, 15.99);
+    }
+
+    @Test
+    public void hasDescription(){
+        assertEquals("Footrest", other.getDescription());
+    }
+
+    @Test
+    public void hasBuyPrice(){
+        assertEquals(9.30, other.getBuyPrice(), 0.01);
+    }
+
+    @Test
+    public void hasSellPrice(){
+        assertEquals(15.99, other.getSellPrice(), 0.01);
+    }
+
+    @Test
+    public void hasMarkUp(){
+        assertEquals(6.69, other.calculateMarkup(), 0.01);
+    }
 }
