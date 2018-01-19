111730f479dfd7b0ae77b9a20ac6c70d97af4f4c
Mark Blanford <markytorture@hotmail.com>
2018-01-19 15:43:45 +0000

    Refactored Instrument class to make it a subclass of SellableItem

--- src/main/java/Instruments/Instrument.java
+++ src/main/java/Instruments/Instrument.java
@@ -1,39 +1,25 @@
 package Instruments;
 
 import Interfaces.ISellable;
+import Shop.SellableItem;
 
-public abstract class Instrument implements ISellable{
+public abstract class Instrument extends SellableItem implements ISellable{
 
     private InstrumentType instrumentType;
     private String brand;
-    private double buyPrice;
-    private double sellPrice;
 
     public Instrument(InstrumentType instrumentType, String brand, double buyPrice, double sellPrice){
+        super(buyPrice, sellPrice);
         this.instrumentType = instrumentType;
         this.brand = brand;
-        this.buyPrice = buyPrice;
-        this.sellPrice = sellPrice;
     }
 
     public InstrumentType getInstrumentType() {
         return this.instrumentType;
     }
 
-    public String getBrand() {
+    public String getBrand(){
         return this.brand;
     }
 
-    public double getBuyPrice() {
-        return this.buyPrice;
-    }
-
-    public double getSellPrice() {
-        return this.sellPrice;
-    }
-
-    public double calculateMarkup(){
-        double markUp = getSellPrice() - getBuyPrice();
-        return markUp;
-    }
 }
